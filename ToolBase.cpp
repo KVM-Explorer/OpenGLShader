e74b2f4788d9f7e499ae317a4d1a35ed9b1d0f1e
KVM <wfcyywh1225@126.com>
2022-04-04 20:40:59 +0800

    实现随位置移动绘制实时固定UI

--- ToolBase.cpp
+++ ToolBase.cpp
@@ -1,2 +1,35 @@
 #include "pch.h"
 #include "ToolBase.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtx/vector_angle.hpp>
+using vec2 = glm::vec2;
+
+mat4 ToolBase::CalculateModelMaxtrix(vec3 view_pos, vec3 view_head)
+{
+	mat4 model_mat;
+	vec3 pos,rotate_vector;
+	float ratio = 10;
+	pos.x = view_pos.x + view_head.x * ratio;
+	pos.y = view_pos.y + view_head.y * ratio;
+	pos.z = view_pos.z + view_head.z * ratio;
+	model_mat = glm::translate(pos);
+
+
+	rotate_vector = glm::cross(view_head, vec3(0.f, 1.f, 0.f));
+	auto angle1 = glm::pi<float>() / 2.f - glm::angle(view_head, vec3(0.f, 1.f, 0.f));
+	model_mat = model_mat * glm::rotate(angle1, rotate_vector);
+
+
+
+	auto angle2 = abs(glm::angle(glm::normalize(vec2(view_head.x,view_head.z)),glm::normalize(vec2(0.f,-1.f))));	//x z ÈÆYÖáÐý×ª
+	if (view_head.x > 0)angle2 = - angle2;
+	//if (view_head.x < 0) angle = -angle;
+	model_mat = model_mat * glm::rotate(angle2, vec3(0.f, 1.f, 0.f));
+	
+	//TRACE("X: %.2f Z: %.2f Y: %.2f Angle1:%.2f Angle2:%.2f\n", view_head.x,view_head.z,view_head.y, angle1/3.14*180.f,angle2/3.14f*180.f);
+
+
+	return model_mat;
+}
--- ToolBase.h
+++ ToolBase.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <gl/glew.h>
+#include <glm/glm.hpp>
+using vec3 = glm::vec3;
+using mat4 = glm::mat4;
+
 class ToolBase
 {
+
+protected:
+	vec3 offset;
+
+
+public:
+	mat4 CalculateModelMaxtrix(vec3 view_pos, vec3 view_head);
+	
+
 };
 
