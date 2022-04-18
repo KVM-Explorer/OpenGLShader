df9d16ccf5845512efae458ee7672c232cc1a810
KVM <wfcyywh1225@126.com>
2022-04-18 20:01:41 +0800

    更改项目配置，尝试网格细化

--- MeshManager.cpp
+++ MeshManager.cpp
@@ -9,6 +9,7 @@ MeshManager::MeshManager()
 	stX = stY = stZ = 0;
 	renderMode = ModeType::single;
 	centerPos = vec3(-3600.f, -3750.f, -804.f);
+	refineFlag = false;
 }
 
 MeshManager::~MeshManager()
@@ -17,16 +18,10 @@ MeshManager::~MeshManager()
 
 void MeshManager::init(DS data_struct)
 {
-	dataStructure = data_struct;
+	dataStructure = data_struct;		// 数据规模定义
 	int st = 0;
-	float max_x(0), max_y(0), max_z(0);
 
-	auto max = [](float a, float b)
-	{
-		if (a < b)return b;
-		return a;
-	};
-
+	units.clear();
 	for(int z=0;z<dataStructure.z;z++)
 		for(int y=0;y<dataStructure.y;y++)
 			for (int x = 0; x < dataStructure.x; x++)
@@ -35,15 +30,40 @@ void MeshManager::init(DS data_struct)
 				unit->init(dataStructure.buffer,st);
 				units.emplace_back(unit);
 				st += 24;
-				for (int k = 0; k < 24; k+=3)
-				{
-					max_x = max(max_x, dataStructure.buffer[st + k]);
-					max_y = max(max_y, dataStructure.buffer[st + k + 1]);
-					max_z = max(max_z, dataStructure.buffer[st + k + 2]);
-
-				}
 			}
-	TRACE("MAX %.2f %.2f %.2f", max_x, max_y, max_z);
+	//TRACE("MAX %.2f %.2f %.2f", max_x, max_y, max_z);
+}
+
+void MeshManager::initRefineMesh(DS data_struct, int multiple)
+{
+	units.clear();
+	dataStructure = data_struct;
+	int expand_ratio = pow(2, multiple);
+	units = std::vector<std::shared_ptr<Unit>>(dataStructure.num*expand_ratio*expand_ratio*expand_ratio);
+	// 数据范围扩充
+	dataStructure.x = dataStructure.x * expand_ratio;
+	dataStructure.y = dataStructure.y * expand_ratio;
+	dataStructure.z = dataStructure.z * expand_ratio;
+	dataStructure.num = dataStructure.x * dataStructure.y * dataStructure.z;
+
+	int dx, dy, dz;
+	int st = 0;
+	for (int z = 0; z < data_struct.z; z++)
+		for (int y = 0; y < data_struct.y; y++)
+			for (int x = 0; x < data_struct.x; x++)	// 遍历block
+			{
+				int ux = data_struct.buffer[st + 3] - data_struct.buffer[st];
+				int uy = data_struct.buffer[st + 6 + 1] - data_struct.buffer[st + 3 + 1];
+				int uz = data_struct.buffer[st + 12 + 2] - data_struct.buffer[st + 2];
+			
+				for(int u=0;u<expand_ratio;u++)
+					for(int v=0;v<expand_ratio;v++)
+						for (int t = 0; t < expand_ratio; t++)	// 细化block
+						{
+							//dx = x + 
+						}
+			}
+
 }
 
 
@@ -54,13 +74,14 @@ void MeshManager::render()
 	else
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	
-	for(int z= stZ;z<dataStructure.z;z++)
-		for(int y=stY;y<dataStructure.y;y++)
+	for (int z = stZ; z < dataStructure.z; z++)
+		for (int y = stY; y < dataStructure.y; y++)
 			for (int x = stX; x < dataStructure.x; x++)
 			{
-				int index = x + y*dataStructure.x+ z * dataStructure.x * dataStructure.y;
+				int index = x + y * dataStructure.x + z * dataStructure.x * dataStructure.y;
 				units[index]->render();
 			}
+
 }
 
 void MeshManager::setProperty(PS property_struct)
@@ -127,14 +148,8 @@ void MeshManager::setProperty(PS property_struct)
 	}
 }
 
-void MeshManager::setProjection(mat4 proj)
+void MeshManager::setRefineProperty(PS property_struct)
 {
-	projection = proj;
-}
-
-void MeshManager::setViewMatrix(mat4 view_matrix)
-{
-	viewMatrix = view_matrix;
 }
 
 void MeshManager::selectViewRange(int dimension,int st)
